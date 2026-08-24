9fd0ebe75f6414c07c4c80f84f8d4c169b120996
Yehor Radykop <egorradikop@gmail.com>
2026-08-24 12:49:15 +0200

    Add route guard

--- task-management-client/src/app/app.routes.ts
+++ task-management-client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Projects } from './pages/projects/projects';
+import { authGuard } from './guards/auth-guard';
+import { guestGuard } from './guards/guest-guard';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: 'projects', component: Projects },
+  { path: 'login', component: Login, canActivate: [guestGuard] },
+  { path: 'projects', component: Projects, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
